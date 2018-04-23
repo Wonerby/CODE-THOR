29dc42ebd12246f9a469a0bf27ca696c8ecfbe8c
Wonerby <wonerby@outlook.com>
2018-04-23 23:58:17 +0800

    START to learning spring boot

---
+++ thor/src/main/java/com/wonerby/thor/ThorApplication.java
@@ -0,0 +1,12 @@
+package com.wonerby.thor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThorApplication.class, args);
+    }
+}
